e031acedd61924c66a3405d12941db1bc8ed6437
Oh Kai Jie <ohkaijie@gmail.com>
2016-07-21 12:45:18 +0800

    Add files via upload
    Added CSS and javascripts library files

---
+++ js/custom.js
@@ -0,0 +1,18 @@
+
+
+$(document).ready(function() {
+	
+
+	$("#specialise").html("Electrical Engineer - Electronic Engineer - Web Developer");
+	$("#description1").html("I studied in Bachelor of Engineering (Electrical and Electronic Engineering) at Swinburne University of Technology, self-learned as a web devlopers. I loved programming, including hardware as well as software based.");
+	$("#description2").html("<b>Skills:</b> <br>C, C++, assembly, Matlab, HTML5, CSS3, Javascript, JQuery");
+	$("#address-location").html("86000 Kluang, Johor <br>Malaysia");
+
+	
+	$("#portfolio-item-1").prop("disabled", true);
+	$("#portfolio-item-2").empty();
+	$("#portfolio-item-3").empty();
+	$("#portfolio-item-4").empty();
+	$("#portfolio-item-5").empty();
+	$("#portfolio-item-6").empty();
+});
